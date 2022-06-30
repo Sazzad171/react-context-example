9cc6ed681059bc162f565ef3637f3757d1f034a3
Sazzad171 <sajjad.cse.diu@gmail.com>
2022-06-30 19:08:59 +0600

    update

--- src/App.js
+++ src/App.js
@@ -11,9 +11,21 @@ function App() {
 
   // state
   const [users, setUsers] = useState([{id: 1, name: "sazzad", age: 24, bio: "Engineer"}]);
+  const [showEditModal, setShowUserModal] = useState(false);
+
+  // user event function
+  const dispatchUserEvent = (actionType, payload) => {
+    switch(actionType) {
+      case 'Remover_user':
+        setUsers(users.filter( user => user.id !== payload.userId));
+        return;
+      default:
+        return;
+    }
+  }
 
   return (
-    <AppContext.Provider value={{ users }}>
+    <AppContext.Provider value={{ users, showEditModal, setShowUserModal, dispatchUserEvent }}>
       <AddUser />
       <UserList />
     </AppContext.Provider>
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import { Col, Card } from 'react-bootstrap';
-// import { AppContext } from '../context/context';
+import React, { useContext } from 'react';
+import { Col, Card, Modal } from 'react-bootstrap';
+import { AppContext } from '../context/context';
 
 export default function User({ user }) {
+
+  // useContext
+  const { showEditModal, setShowUserModal, dispatchUserEvent } = useContext(AppContext);
+  
   return (
     <Col md={4}>
       <Card>
@@ -13,10 +17,18 @@ export default function User({ user }) {
             <br />
             { user.bio }
           </Card.Text>
-          <a href="#f" className='text-primary pe-3'>Edit</a>
-          <a href="#f" className='text-danger'>Delete</a>
+          <a href="#f" className='text-primary pe-3' onClick={ () => setShowUserModal(true) }>Edit</a>
+          <a href="#fg" className='text-danger' onClick={() => { dispatchUserEvent('Remover_user', { userId: user.id }) }}>Delete</a>
         </Card.Body>
       </Card>
+
+      {/* modal */}
+      <Modal show={ showEditModal } onHide={ () => setShowUserModal(false) }>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit user data of {user.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      </Modal>
     </Col>
   )
 }
